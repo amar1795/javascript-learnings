f7033e1e0612dc1f5a148fad995c8713560a987f
Amar <jeetamar496@gmail.com>
2023-07-06 01:32:01 +0530

    deleteitem logic added

--- javascript mini dom project/index.js
+++ javascript mini dom project/index.js
@@ -1,6 +1,8 @@
 let sbtn=document.getElementById('sbtn');
 let ullist=document.getElementById('ullist');
 
+ullist.addEventListener('click',deletefunction);
+
 
 sbtn.addEventListener('click',func)
 
@@ -12,10 +14,27 @@ function func(e) {
     let li=document.createElement('li');
     li.className='list-group-item';
     let delete_button=document.createElement('button');
-    delete_button.className='btn btn-danger float-end';
+    delete_button.className='btn btn-danger float-end delete';
     delete_button.appendChild(document.createTextNode('X'));
     li.appendChild(delete_button);
     li.appendChild(document.createTextNode(maininput));
     ullist.appendChild(li);
+}
+
+
+
+function deletefunction(e) {
+
+    if(e.target.classList.contains('delete'))
+    {
+
+        console.log("you have clicked on delete")
+        if(confirm('are you sure'))
+        {
+            let parent_element=e.target.parentElement;
+            ullist.removeChild(parent_element);
+        }
+    }
+    
 }
 
